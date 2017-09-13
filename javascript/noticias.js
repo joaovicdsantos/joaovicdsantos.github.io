b3f63d4b7a9515234ff7b72617357e8661fb0ead
RaolootneXII <jv20252025@gmail.com>
2017-09-12 22:19:55 -0300

    Commit Automatico

--- javascript/noticias.js
+++ javascript/noticias.js
@@ -10,6 +10,27 @@ function iniciar(){
 
 function trocarNoticia(){
 	if (fi == 1) {
+		document.getElementById('noticiaT').innerHTML = 'Ultimos Posts - 17:37 (Topem)';
+		document.getElementById('noticiaP').innerHTML = 'Tudo comecou normal, em 2016, um '+
+		'pouco mais que a metade do ano, para ser mais exata, nada fora do lugar, acordei, '+
+		'escovei os dentes, tomei café, banho, almocei e várias coisas no meio tempo dos '+
+		'mesmos, e exatamente 12:30 lá estava eu pegando a bicicleta e indo para o colégio...';
+		document.getElementById('noticiaL').href = 'posts/2017/setembro/17:37.html';
+		document.getElementById('noticiaN').innerHTML = '1/3';
+		document.getElementById('noticiaF').src = 'imagens/n1.jpg';
+	}
+	else if (fi == 2){
+		document.getElementById('noticiaT').innerHTML = 'Ultimos Posts - Dia do Led (Bra)';
+		document.getElementById('noticiaP').innerHTML = 'Esse dia é mais um exemplo de que a '+
+		'felicidade está nas pequenas coisas do dia e se a gente não perceber isso acaba '+
+		'tentando buscar a felicidade em outras coisas. Bom nós, eu e 4 pessoas da minha '+
+		'equipe, tínhamos um trabalho de química, o objetivo era acender um simples Led com '+
+		'batata e limão, mas, nós não tínhamos o Led então tivemos que comprar....';
+		document.getElementById('noticiaL').href = 'posts/2017/setembro/led.html';
+		document.getElementById('noticiaN').innerHTML = '2/3';
+		document.getElementById('noticiaF').src = 'imagens/n2.jpg';
+	}
+	else if (fi == 3){
 		document.getElementById('noticiaT').innerHTML = 'Ultimos Posts - Mudanças!! (Bra)';
 		document.getElementById('noticiaP').innerHTML = 'Para aumentar o conteúdo do site, '+
 		'mudanças precisam acontecer, foi pensando nisso que agora nosso site é administrado '+ 
@@ -17,28 +38,6 @@ function trocarNoticia(){
 		'personalidade, que pode ser acessado pelo menu logo a cima, explicando algumas coisas '+ 
 		'e servindo de introdução para, basicamente...';
 		document.getElementById('noticiaL').href = 'posts/2017/setembro/mudancas.html';
-		document.getElementById('noticiaN').innerHTML = '1/3';
-		document.getElementById('noticiaF').src = 'imagens/n1.jpg';
-	}
-	else if (fi == 2){
-		document.getElementById('noticiaT').innerHTML = 'Ultimos Posts - Mudou (Bra)';
-		document.getElementById('noticiaP').innerHTML = '"Ultimamente nada, nada me '+
-		'interessa. Nem uma conversa. Eu não sinto pressa, saudade ou paixão...". '+
-		'Talvez essa seja a música que mais faz sentido na minha vida no momento '+
-		'atual. Antigamente parecia legal a ideia de estar junto de outras pessoas '+ 
-		'compartilhando histórias, hoje não vejo prazer algum nisso...';
-		document.getElementById('noticiaL').href = 'posts/2017/setembro/mudou.html';
-		document.getElementById('noticiaN').innerHTML = '2/3';
-		document.getElementById('noticiaF').src = 'imagens/n2.jpg';
-	}
-	else if (fi == 3){
-		document.getElementById('noticiaT').innerHTML = 'Ultimos Posts - Folclore (Bra)';
-		document.getElementById('noticiaP').innerHTML = 'O dia começou as 5 horas da manhã, '+
-		'o sono me consumia, lembro de ter sonhado, mas, não lembro com o que. O objetivo era '+ 
-		'que todos da minha equipe estivessem na escola de 6 horas, sendo que a escola abre as '+ 
-		'7 horas, para que nós pudéssemos ensaiar. O problema foi que só chegou eu e mais uma '+
-		'amiga, os outros 3 não vieram, além disso...';
-		document.getElementById('noticiaL').href = 'posts/2017/agosto/diafolclore.html';
 		document.getElementById('noticiaN').innerHTML = '3/3';
 		document.getElementById('noticiaF').src = 'imagens/n3.jpg';
 	}
